5d142bc7d8172d6b853f6708b6393501c413151e
Abdurahmonbek <abdurahmonkodirov97@gmail.com>
2022-10-24 00:02:13 +0500

    Added Request Mutators

--- src/Framework/Http/Request.php
+++ src/Framework/Http/Request.php
@@ -4,22 +4,26 @@ namespace Framework\Http;
 
 class Request
 {
-    private array $queryParams;
-    private mixed $parsedBody;
-
-    public function __construct(array $queryParams = [], $parsedBody = null)
-    {
-        $this->queryParams = $queryParams;
-        $this->parsedBody = $parsedBody;
-    }
+    private array $queryParams = [];
+    private mixed $parsedBody = null;
 
     public function getQueryParams(): array
     {
         return $this->queryParams;
     }
 
+    public function withQueryParams(array $query): void
+    {
+        $this->queryParams = $query;
+    }
+
     public function getParsedBody(): ?array
     {
         return $this->parsedBody;
+    }
+
+    public function withParsedBody(array $data): void
+    {
+        $this->parsedBody = $data;
     }
 }
