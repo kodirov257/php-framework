bd94d07b7c138d991144d3793e88fde7179f5711
Abdurahmonbek <abdurahmonkodirov97@gmail.com>
2022-10-24 00:45:18 +0500

    Added Response

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Framework\Http\RequestFactory;
+use Framework\Http\Response;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
@@ -12,5 +13,15 @@ $request = RequestFactory::fromGlobals();
 ### Action
 
 $name = $request->getQueryParams()['name'] ?? 'Guest';
-header('X-Developer: Abdurakhmon Kodirov');
-echo 'Hello, ' . $name . '!';
+
+$response = (new Response('Hello, ' . $name . '!'))
+    ->withHeader('X-Developer', 'Abdurakhmon Kodirov');
+
+### Sending
+
+header('HTTP/1.0 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+foreach ($response->getHeaders() as $name => $value) {
+    header($name . ':' . $value);
+}
+
+echo $response->getBody();
---
+++ src/Framework/Http/Response.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Framework\Http;
+
+class Response
+{
+    private array $headers = [];
+    private mixed $body;
+    private mixed $statusCode;
+    private string $reasonPhrase = '';
+
+    private static array $phrases = [
+        200 => 'OK',
+        301 => 'Moved Permanently',
+        400 => 'Bad Request',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        500 => 'Internal Server Error',
+    ];
+
+    public function __construct(mixed $body, $status = 200)
+    {
+        $this->body = $body;
+        $this->statusCode = $status;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function withBody(mixed $body): self
+    {
+        $new = clone $this;
+        $new->body = $body;
+        return $new;
+    }
+
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    public function getReasonPhrase(): string
+    {
+        if (!$this->reasonPhrase && isset(self::$phrases[$this->statusCode])) {
+            $this->reasonPhrase = self::$phrases[$this->statusCode];
+        }
+        return $this->reasonPhrase;
+    }
+
+    public function withStatus(mixed $code, string $reasonPhrase = ''): self
+    {
+        $new = clone $this;
+        $new->body = $code;
+        $new->reasonPhrase = $reasonPhrase;
+        return $new;
+    }
+
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
+
+    public function hasHeader(string $header): bool
+    {
+        return isset($this->headers[$header]);
+    }
+
+    public function getHeader(string $header)
+    {
+        if (!$this->hasHeader($header)) {
+            return null;
+        }
+        return $this->headers[$header];
+    }
+
+    public function withHeader(string $header, mixed $value): self
+    {
+        $new = clone $this;
+        if ($new->hasHeader($header)) {
+            unset($this->headers[$header]);
+        }
+        $new->headers[$header] = $value;
+        return $new;
+    }
+
+}
