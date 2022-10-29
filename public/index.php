1fe0ec548d9e4d1663f88d107ef79a1acc0e00de
Abdurahmonbek <abdurahmonkodirov97@gmail.com>
2022-10-29 13:53:40 +0500

    Added Blog actions

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Laminas\Diactoros\Response\HtmlResponse;
+use Laminas\Diactoros\Response\JsonResponse;
 use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 
@@ -20,6 +21,18 @@ if ($path === '/') {
     $response = (new HtmlResponse('Hello, ' . $name . '!'));
 } elseif ($path === '/about') {
     $response = new HtmlResponse('I am a simple site');
+} elseif ($path === '/blog') {
+    $response = new JsonResponse([
+        ['id' => 2, 'title' => 'Second Post'],
+        ['id' => 1, 'title' => 'First Post'],
+    ]);
+} elseif (preg_match('#^/blog/(?P<id>\d+)$#i', $path, $matches)) {
+    $id = $matches['id'];
+    if ($id > 2) {
+        $response = new JsonResponse(['error' => 'Undefined page']);
+    } else {
+        $response = new JsonResponse(['id' => $id, 'post' => 'Post #' . $id]);
+    }
 } else {
     $response = new HtmlResponse('Undefined page', 404);
 }
