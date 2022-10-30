00a0c397a78a7826d1e38d7b2afe94e4c13bf529
Abdurahmonbek <abdurahmonkodirov97@gmail.com>
2022-10-30 14:29:53 +0500

    Incapsualated Route behavior

--- src/Framework/Http/Router/Route.php
+++ src/Framework/Http/Router/Route.php
@@ -2,6 +2,8 @@
 
 namespace Framework\Http\Router;
 
+use Psr\Http\Message\ServerRequestInterface;
+
 class Route
 {
     public string $name;
@@ -17,5 +19,47 @@ class Route
         $this->handler = $handler;
         $this->methods = $methods;
         $this->tokens = $tokens;
+    }
+
+    public function match(ServerRequestInterface $request): ?Result
+    {
+        if ($this->methods && !in_array($request->getMethod(), $this->methods, true)) {
+            return null;
+        }
+
+        $pattern = preg_replace_callback('~\{([^\}]+)\}~', function ($matches) {
+            $argument = $matches[1];
+            $replace = $this->tokens[$argument] ?? '[^}]+';
+            return '(?P<' . $argument . '>' . $replace . ')';
+        }, $this->pattern);
+
+        $path = $request->getUri()->getPath();
+
+        if (!preg_match('~^' . $pattern . '$~i', $path, $matches)) {
+            return null;
+        }
+
+        return new Result(
+            $this->name,
+            $this->handler,
+            array_filter($matches, '\is_string', ARRAY_FILTER_USE_KEY)
+        );
+    }
+
+    public function generate($name, array $params = []): ?string
+    {
+        $arguments = array_filter($params);
+
+        if ($name != $this->name) {
+            return null;
+        }
+
+        return preg_replace_callback('~\{([^\}]+)\}~', function ($matches) use (&$arguments) {
+            $argument = $matches[1];
+            if (!array_key_exists($argument, $arguments)) {
+                throw new \InvalidArgumentException('Missing parameter "' . $argument . '"');
+            }
+            return $arguments[$argument];
+        }, $this->pattern);
     }
 }
