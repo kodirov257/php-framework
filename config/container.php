599e344d0c9bfe6d2112dc3ad746890491df19cf
Abdurahmonbek <abdurahmonkodirov97@gmail.com>
2022-12-26 23:21:11 +0500

    Added Container constructor definitions

--- config/container.php
+++ config/container.php
@@ -1,12 +1,9 @@
 <?php
 
-use App\Http\Middlewares;
 use Framework\Container\Container;
 
-$container = new Container();
+$container = new Container(require __DIR__ . '/dependencies.php');
 
 $container->set('config', require __DIR__ . '/parameters.php');
-
-require __DIR__ . '/dependencies.php';
 
 return $container;
--- config/dependencies.php
+++ config/dependencies.php
@@ -8,36 +8,37 @@ use Framework\Http\MiddlewareResolver;
 use Framework\Http\Router\Router;
 use Laminas\Diactoros\Response;
 
-/* @var $container Framework\Container\Container */
-$container->set(Middlewares\BasicAuthMiddleware::class, function (Container $container) {
-    return new Middlewares\BasicAuthMiddleware($container->get('config')['users']);
-});
+return [
+    Middlewares\BasicAuthMiddleware::class => function (Container $container) {
+        return new Middlewares\BasicAuthMiddleware($container->get('config')['users']);
+    },
 
-$container->set(Middlewares\ErrorHandlerMiddleware::class, function (Container $container) {
-    return new Middlewares\ErrorHandlerMiddleware($container->get('config')['debug']);
-});
+    Middlewares\ErrorHandlerMiddleware::class => function (Container $container) {
+        return new Middlewares\ErrorHandlerMiddleware($container->get('config')['debug']);
+    },
 
-$container->set(Router::class, function (Container $container) {
-    return new Router();
-});
+    Router::class => function (Container $container) {
+        return new Router();
+    },
 
-$container->set(Application::class, function (Container $container) {
-    $notFoundHandler = config('app.not_found_handler') ?? Framework\Http\Middleware\NotFoundHandler::class;
-    return new Application($container->get(MiddlewareResolver::class), new $notFoundHandler());
-});
+    Application::class => function (Container $container) {
+        $notFoundHandler = config('app.not_found_handler') ?? Framework\Http\Middleware\NotFoundHandler::class;
+        return new Application($container->get(MiddlewareResolver::class), new $notFoundHandler());
+    },
 
-$container->set(MiddlewareResolver::class, function (Container $container) {
-    return new MiddlewareResolver(new Response(), $container);
-});
+    MiddlewareResolver::class => function (Container $container) {
+        return new MiddlewareResolver(new Response(), $container);
+    },
 
-$container->set(ActionResolver::class, function (Container $container) {
-    return new ActionResolver($container);
-});
+    ActionResolver::class => function (Container $container) {
+        return new ActionResolver($container);
+    },
 
-$container->set(Middlewares\CredentialsMiddleware::class, function () {
-    return new Middlewares\CredentialsMiddleware();
-});
+    Middlewares\CredentialsMiddleware::class => function () {
+        return new Middlewares\CredentialsMiddleware();
+    },
 
-$container->set(Middlewares\ProfilerMiddleware::class, function () {
-    return new Middlewares\ProfilerMiddleware();
-});
+    Middlewares\ProfilerMiddleware::class => function () {
+        return new Middlewares\ProfilerMiddleware();
+    }
+];
