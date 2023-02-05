745374f39ab75ffedf8508b5bc89dc1c99229920
Abdurahmonbek <abdurahmonkodirov97@gmail.com>
2023-02-05 23:06:53 +0500

    Added Extension Options

--- src/Framework/Template/Php/Extension.php
+++ src/Framework/Template/Php/Extension.php
@@ -4,6 +4,9 @@ namespace Framework\Template\Php;
 
 abstract class Extension
 {
+    /**
+     * @return ExtensionFunction[]
+     */
     public function getFunctions(): array
     {
         return [];
